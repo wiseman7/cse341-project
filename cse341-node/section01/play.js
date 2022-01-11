f87e63e6f2739823ff01059ac480656104d018fe
wiseman7 <wis16002@byui.edu>
2022-01-11 12:40:48 -0700

    commit now

--- cse341-node/section01/play.js
+++ cse341-node/section01/play.js
@@ -1,16 +1,22 @@
-const person = {
-    name: 'Max',
-    age: 29,
-    greet() {
-        console.log('Hi, I am ' + this.name);
-    }
+const fetchData = () => {
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Done!');
+        }, 1500);
+    });  
+    return promise;
 };
 
-const hobbies = ['Sports', 'Cooking'];
-// for (let hobby of hobbies) {
-//     console.log(hobby);
-// }
-// console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
-// console.log(hobbies);
-hobbies.push('Programming');
-console.log(hobbies);
+setTimeout(() => {
+    console.log('Timer is done!');
+    fetchData().then(text => {
+        console.log(text);
+        return fetchData();
+    })
+    .then(text2 => {
+        console.log(text2);
+    });
+}, 2000);
+
+console.log('Hello!');
+console.log('Hi!');
