16910ef19d2ddabc2b3d75d8a69cd6b0245acadc
wiseman7 <wis16002@byui.edu>
2022-01-12 23:25:03 -0700

    Week 2 activity

--- routes/ta02.js
+++ routes/ta02.js
@@ -3,10 +3,38 @@
 const express = require('express');
 const router = express.Router();
 
+// Step 01
+const userList = [Rick, John, Bob];
+
+// Step 02
+router.post('/addUser', (req, res, next) => {
+  const newPerson = req.body.newPerson;
+
+  userList.push(newPerson);
+
+  res.redirect('/ta02');
+});
+
+// Step 03
+router.post('/removeUser', (req, res, next) => {
+  const remPerson = req.body.remPerson;
+
+  // With it being a const it is harder to remove things. you need to use a splice method to remove from a 
+  // const array.
+  const index = userList.indexOf(remPerson);
+  if (index !== -1 ) {
+    userList.splice(index, 1);
+  }
+
+  res.redirect('/ta02/');
+})
+
+
 router.get('/', (req, res, next) => {
-  res.render('pages', {
+  res.render('pages/ta02', {
     title: 'Team Activity 02',
     path: '/ta02', // For pug, EJS
+    names: userList,
     activeTA03: true, // For HBS
     contentCSS: true, // For HBS
   });
